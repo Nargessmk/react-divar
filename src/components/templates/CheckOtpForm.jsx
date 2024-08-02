4bafc9d294d925256162e249ff4e39cca0830307
Narges Makvandi <narges.makvandi@yahoo.com>
2024-08-02 15:55:38 +0330

    initial commit

---
+++ src/components/templates/CheckOtpForm.jsx
@@ -0,0 +1,32 @@
+import { checkOtp } from "../../services/auth";
+import { setCookie } from "../../utils/cookie"
+
+function CheckOtpForm({ code, setCode, setStep, mobile }) {
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    if (code.length !== 5) return;
+    const { response, error } = await checkOtp(mobile, code);
+    if (response) setCookie(response.data);
+    if (error) console.log(error.response.data.message);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <p>تایید کد پیامک شده</p>
+      <p>کد پیامک شده به شماره "{mobile}" را وارد کنید</p>
+      <label htmlFor="">کد تایید را وارد کنید</label>
+      <input
+        type="text"
+        name="code"
+        id="input"
+        placeholder="کد تایید"
+        value={code}
+        onChange={(e) => setCode(e.target.value)}
+      />
+      <button type="submit">ورود</button>
+      <button onClick={() => setStep(1)}>تغییر شماره موبایل</button>
+    </form>
+  );
+}
+
+export default CheckOtpForm;
